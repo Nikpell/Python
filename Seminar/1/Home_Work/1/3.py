6618a1db321fea29f0d01e70ef4f8ae90669b8fe
Nikolay <popov0772@gmail.ru>
2022-09-03 23:43:13 +0500

    finished third task

--- Seminar/1/Home_Work/1/3.py
+++ Seminar/1/Home_Work/1/3.py
@@ -5,5 +5,15 @@
 # - x=34; y=-30 -> 4
 # - x=2; y=4-> 1
 # - x=-34; y=-30 -> 3
-lst = [i for i in range(-5, 6)]
-print(len(lst))
+
+x_coordinate = int(input('Введите координату точки по оси X: '))
+y_coordinate = int(input('Введите координату точки по оси Y: '))
+if x_coordinate > 0 and y_coordinate > 0:
+    quarter = 1
+elif x_coordinate > 0 > y_coordinate:
+    quarter = 4
+elif x_coordinate < 0 and y_coordinate < 0:
+    quarter = 3
+else:
+    quarter = 2
+print(f'Точка находится в {quarter} четверти плоскости.')
