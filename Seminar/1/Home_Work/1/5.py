f49a446faef51f86c4780118364a90f3a871c41b
Nikolay <popov0772@gmail.ru>
2022-09-06 17:32:49 +0500

    finished all tasks

--- Seminar/1/Home_Work/1/5.py
+++ Seminar/1/Home_Work/1/5.py
@@ -1,6 +1,14 @@
+import math
+
 # Напишите программу, которая принимает на вход координаты двух точек и находит
 # расстояние между ними в 2D пространстве.
 # Пример:
 # - A (3,6); B (2,1) -> 5,09
 # - A (7,-5); B (1,-1) -> 7,21
 
+string_A = input('Введите координаты точки А через запятую: ')
+string_B = input('Введите координаты точки B через запятую: ')
+coordinate_A = [int(string_A.split(',')[0]), int(string_A.split(',')[1])]
+coordinate_B = [int(string_B.split(',')[0]), int(string_B.split(',')[1])]
+distance_A_to_B = math.sqrt((coordinate_A[0] - coordinate_B[0]) ** 2 + (coordinate_A[1] - coordinate_B[1]) ** 2)
+print(f'Расстояние от точки А до точки В = {distance_A_to_B}')
