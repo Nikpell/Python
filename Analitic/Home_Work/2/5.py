3a296ce66df0a98c48136b4afc195d51f51fcd1c
Nikolay <popov0772@gmail.ru>
2022-09-07 23:42:03 +0500

    finished all tasks

---
+++ Analitic/Home_Work/2/5.py
@@ -0,0 +1,23 @@
+subsecquence = []
+number = int(input('Введите количество элементов:'))
+for i in range(1, number + 1):
+    subsecquence.append(i)
+for i in range(1, number):
+    index = 0
+    index_2 = index + i
+    if i // 2 == 0:
+        while index + index_2 < len(subsecquence):
+            temp = subsecquence[index]
+            subsecquence[index] = subsecquence[index_2]
+            subsecquence[index_2] = temp
+            index = index_2 + 1
+            index_2 += (i + 1)
+    else:
+        index = 1
+        index_2 = index + i
+        while index + index_2 < len(subsecquence):
+            temp = subsecquence[-index]
+            subsecquence[-index] = subsecquence[-index_2]
+            subsecquence[-index_2] = temp
+            index = index_2 + 1
+            index_2 += (i + 1)
