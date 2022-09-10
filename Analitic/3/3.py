5128edfb1f67fc0b9bc716db3173fdbbc8f9646d
Nikolay <popov0772@gmail.ru>
2022-09-10 17:10:02 +0500

    finished all tasks

--- Analitic/3/3.py
+++ Analitic/3/3.py
@@ -4,3 +4,11 @@
 # Сергей Сердюк: # Тесты
 # print(to_dict([1, 2, 3, 4]))
 # print(to_dict(['grey', (2, 17), 3.11, -4]))
+
+def to_dict(lst):
+    return {element: element for element in lst}
+
+
+# Тесты
+print(to_dict([1, 2, 3, 4]))
+print(to_dict(['grey', (2, 17), 3.11, -4]))
