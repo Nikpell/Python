5f00c348b6400da7d7474b8c2a25c9836aa51fa8
Nikolay <popov0772@gmail.ru>
2022-09-12 15:04:36 +0500

    finished all tasks

--- Seminar/4/2.py
+++ Seminar/4/2.py
@@ -1,2 +1,16 @@
 # 3. Задайте два числа. Напишите программу, которая найдёт НОК
 # (наименьшее общее кратное) этих двух чисел. Ответ записать в файл.
+
+def greatest_common_multiple(number1, number2):
+    multiplication = number1 * number2
+    while number1 != number2:
+        if number1 > number2:
+            number1 = number1 - number2
+        else:
+            number2 = number2 - number1
+    return multiplication / number1
+
+
+with open('task1.txt', 'w') as data:
+    data.write(str(greatest_common_multiple(5, 7)))
+data.close()
