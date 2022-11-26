8acbcbd2a3d0f6e7d0f0c429d204652fbed1aa47
Nikolay <popov0772@gmail.ru>
2022-11-26 12:55:59 +0500

    changed first task

--- Analitic/Home_Work/2_2/1.py
+++ Analitic/Home_Work/2_2/1.py
@@ -7,15 +7,16 @@ def summa_digit_one_string(number: str):
     return sum(list(map(int, filter(lambda i: i.isdigit(), [x for x in number]))))
 
 # Через математику делать костыльно и все равно со строкой, на float особенности хранения сильно вмешивается,
-# def summa_digit_math(number: float):
-#     summa = 0
-#     while number != 0:
-#         if number % 1 != 0:
-#             number *= 10
-#         else:
-#             summa += number % 10
-#             number //= 10
-#     return summa
+def summa_digit_math(number: float):
+    length = len(str(number))
+    summa = 0
+    while number != 0:
+        if number % 1 != 0:
+            number = round(number * 10, length)
+        else:
+            summa += number % 10
+            number = round(number // 10, length)
+    return summa
 
 # number = input('Введите число: ')
 # print(f'Сумма цифр числа {number} равна {summa_digit_one_string(number)}')
