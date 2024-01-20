2025e2aeb77bcdd46c77640eb1d6f1bb817c2b54
Nikolay <popov0772@gmail.ru>
2024-01-20 16:34:11 +0600

    seminar

---
+++ immersion_in_python/office_work/6.py
@@ -0,0 +1,7 @@
+import guess
+from riddle import answer, ask_riddle_dict
+
+# string = input('Введите от одного до трех чисел(мин, макс, количество попыток) через пробел: ')
+# print(guess.guessfun(string))
+# print(answer('цвет лимона', ['желтый', 'зеленый', 'синий', 'оранжевый'], 3))
+
